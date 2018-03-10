65ca59b787acb389ea549beea3f709c1a19189d0
Vinay Pandey <vinay.pandey12@gmail.com>
2018-03-11 00:11:56 +0530

    function overloading

---
+++ function_overloading.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+/*
+An polymorphism example (fuunction overloading)
+/* <conio.h> is uincluded to use getch 
+*/
+
+float area(int);  /*This is function declaration taking 1 arg (circle)*/
+int  area(int, int); /*This is function declaration taking 2 arg (rectangle)*/
+
+int main(){
+	int r;
+	cout << "Enter radius of circle: ";
+	cin >> r;
+	cout << "Area of circle is: " << area(r);
+	cout <<endl;
+	
+	int l,b;
+
+	cout << "Enter length and breadth of a rectangle: ";
+	cin >> l >> b;
+	cout << "Area of a rectangle is: " <<  area(l,b);
+	cout <<endl;
+}
+
+int area(int a, int b){
+	return a*b;
+}
+
+float area(int r){
+	return (3.14*r*r);
+}
+
